551ee1cdd5b700ff104cc8528d58b930e8577c02
Debjyoti <djbabai.debjyoti@gmail.com>
2024-02-23 16:48:32 +0530

    initial commit

--- arima-model.py
+++ arima-model.py
@@ -1,29 +1,30 @@
 import pandas as pd
+from datetime import datetime
 from statsmodels.tsa.arima.model import ARIMA
-import matplotlib.pyplot as plt
-
-data = pd.read_csv('data/temperature-sensor-data.csv')
-
-time = data["Time"]
-temperature = data["Temperature"]
-
-model = ARIMA(temperature, order=(1, 1, 1))  # Change parameters based on your analysis
-
-# Fit the model
-model_fit = model.fit()
-
-# Predict future values
-future_periods = 10  # Change this to the number of periods to predict
-future_predictions = model_fit.forecast(steps=future_periods)
-
-# Print the predicted values
-print(future_predictions)
 
 
-plt.plot(time, temperature, label='Actual')
-plt.plot(future_predictions, color='red', label='Predicted')
-plt.xlabel('Time')
-plt.ylabel('Temperature')
-plt.title('Temperature Prediction with ARIMA')
-plt.legend()
-plt.show()
+def get_temperature_from_time(data_set_path, time_arg):
+    data = pd.read_csv(data_set_path)
+    time = data["Time"]
+    temperature = data["Temperature"]
+    time_format = " %I:%M:%S %p"  # Format for hours:minutes:seconds AM/PM
+
+    dt1 = datetime.strptime(time[1], time_format)
+    dt2 = datetime.strptime(time[0], time_format)
+
+    time_difference = dt2-dt1
+
+    last_time_stamp = datetime.strptime(data["Time"].iloc[-1], time_format)
+    time_arg = datetime.strptime(time_arg, time_format)
+
+    print(last_time_stamp-time_arg)
+
+    model = ARIMA(temperature, order=(1, 1, 1))  # Change parameters based on your analysis
+
+    model_fit = model.fit()
+    future_periods = 10
+    future_predictions = model_fit.forecast(steps=future_periods)
+
+
+get_temperature_from_time("data/temperature-sensor-data.csv", " 1:50:00 PM")
+
